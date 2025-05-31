d8e14df95155eb97b45a4ffd3ac4f48efa1f7e3e
SIDHARTH M <sidharthm901@gmail.com>
2025-05-31 20:52:15 +0530

    Update message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import qr from "qr-image";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
